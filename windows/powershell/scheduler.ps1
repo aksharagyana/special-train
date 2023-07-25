1446f46599f9e190491ff939d0a0859766cbdbf6
aksharagyana <93100395+aksharagyana@users.noreply.github.com>
2023-07-26 00:52:11 +0100

    Update scheduler.ps1

--- windows/powershell/scheduler.ps1
+++ windows/powershell/scheduler.ps1
@@ -4,10 +4,10 @@ $scriptPath = "C:\Path\To\Your\Script.ps1"
 # Define the action to run the PowerShell script
 $action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File `"$scriptPath`""
 
-# Define the trigger for user logon
-$trigger = New-ScheduledTaskTrigger -AtLogOn -User "<UserName>"
+# Define the trigger for any user logon
+$trigger = New-ScheduledTaskTrigger -AtLogOn -User "AnyUser" -RandomDelay (New-TimeSpan -Minutes 1)
 
 # Create the scheduled task
-Register-ScheduledTask -TaskName "RunScriptOnLogon" -Trigger $trigger -Action $action -User "<UserName>" -RunLevel Highest -Force
+Register-ScheduledTask -TaskName "RunScriptOnLogon" -Trigger $trigger -Action $action -RunLevel Highest -Force
 
-Write-Output "Scheduled task created to run the script on user logon."
+Write-Output "Scheduled task created to run the script on user logon for any user."
