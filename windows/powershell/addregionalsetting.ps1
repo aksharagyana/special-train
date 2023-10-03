e13fae852e26fbf95837821fcf2074a781394bc3
aksharagyana <93100395+aksharagyana@users.noreply.github.com>
2023-10-03 13:31:10 +0100

    Create addregionalsetting.ps1

---
+++ windows/powershell/addregionalsetting.ps1
@@ -0,0 +1,15 @@
+#Set home location to the United Kingdom
+Set-WinHomeLocation 0xf2
+
+#Override language list with just English GB
+$1 = New-WinUserLanguageList en-GB
+$1[0].Handwriting = 1
+Set-WinUserLanguageList $1 -force
+
+#Set system local
+Set-WinSystemLocale en-GB
+
+#Set the timezone
+Set-TimeZone "GMT Standard Time"
+
+#Restart the OS
