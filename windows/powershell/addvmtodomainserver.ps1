53a657fae291138dac204fe7059b1efa14999ce8
aksharagyana <93100395+aksharagyana@users.noreply.github.com>
2023-10-05 10:20:42 +0100

    Update addvmtodomainserver.ps1

--- windows/powershell/addvmtodomainserver.ps1
+++ windows/powershell/addvmtodomainserver.ps1
@@ -6,5 +6,8 @@ $adminPassword = "YourAdminPassword"  # Replace with the domain admin password
 # Specify the credentials for joining the domain
 $credential = New-Object System.Management.Automation.PSCredential($adminUsername, (ConvertTo-SecureString $adminPassword -AsPlainText -Force))
 
-# Join the computer to the domain
-Add-Computer -DomainName $domainName -Credential $credential -Restart -Force
+# Specify the OU path where the computer account should be placed
+$ouPath = "OU=Computers,DC=YourDomain,DC=local"  # Replace with the actual OU path
+
+# Join the computer to the domain and specify the OU path
+Add-Computer -DomainName $domainName -Credential $credential -OUPath $ouPath -Restart -Force
