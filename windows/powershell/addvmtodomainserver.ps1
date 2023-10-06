6e37118728495473adf0ec62f5d9ab51ec390b11
aksharagyana <93100395+aksharagyana@users.noreply.github.com>
2023-10-06 14:15:12 +0100

    Update addvmtodomainserver.ps1

--- windows/powershell/addvmtodomainserver.ps1
+++ windows/powershell/addvmtodomainserver.ps1
@@ -8,6 +8,37 @@ $credential = New-Object System.Management.Automation.PSCredential($adminUsernam
 
 # Specify the OU path where the computer account should be placed
 $ouPath = "OU=Computers,DC=YourDomain,DC=local"  # Replace with the actual OU path
+try {
+# Define the specific IP address of the domain controller
+addTodomain -dcIp "192.168.1.100" "192.168.1.101"
+# # Set the DNS settings to use the specific DC's IP address
+# Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -ServerAddresses $specificDcIp
 
-# Join the computer to the domain and specify the OU path
-Add-Computer -DomainName $domainName -Credential $credential -OUPath $ouPath -Restart -Force
+# # Join the computer to the domain and specify the OU path
+# Add-Computer -DomainName $domainName -Credential $credential -OUPath $ouPath -Restart -Force
+}
+catch {
+   Write-Host "An exception occurred when adding to a domain"
+}
+finally {
+  Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -ResetServerAddresses
+}
+
+
+function addTodomain {
+    param (
+        [string[]]$dcIps
+    )
+    foreach ($dcIp in $dcIps) {
+    try {
+        # Set the DNS settings to use the specific DC's IP address
+        Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).InterfaceIndex -ServerAddresses $dcIp
+      
+        # Join the computer to the domain and specify the OU path
+        Add-Computer -DomainName $domainName -Credential $credential -OUPath $ouPath -Restart -Force
+      }
+      catch {
+        Write-Host "An exception occurred for string '$dcIp': $($_.Exception.Message)"
+      }
+    }
+}
